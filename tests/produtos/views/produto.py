e4e0e39903f993e497cb75e6e94716c27b14d793
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-19 12:01:09 -0300

    criando teste de criar produto sem campos obrigatorios

--- tests/produtos/views/produto.py
+++ tests/produtos/views/produto.py
@@ -1,7 +1,52 @@
 from django.test import TestCase
-from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST
+)
 from apps.produtos.models import Produto
 from apps.produtos.factories import ProdutoFactory, CategoriaFactory
+from parameterized import parameterized
+
+
+DATA_IN_ERROR = [
+    (
+        {
+            'nome': 'Detergente Líquido',
+            'descricao': 'líquido para lavar louças mais pesadas.',
+            'valor': '',
+            'quantidade': 200,
+        },
+        {
+            'campo': 'valor',
+            'msg': 'Um número válido é necessário.'
+        }
+    ),
+    (
+        {
+            'nome': 'Amaciante Líquido',
+            'descricao': 'líquido para perfurmar suas roupas.',
+            'valor': 15.50,
+            'quantidade': '20o',
+        },
+        {
+            'campo': 'quantidade',
+            'msg': 'Um número inteiro válido é exigido.'
+        }
+    ),
+    (
+        {
+            'nome': 'Amaciante Líquido',
+            'descricao': 'líquido para perfurmar suas roupas.',
+            'valor': 15.50,
+            'quantidade': -20,
+        },
+        {
+            'campo': 'quantidade',
+            'msg': 'Um número positivo é exigido.'
+        }
+    )
+]
 
 
 class ProdutoViewSetTestCase(TestCase):
@@ -33,3 +78,13 @@ class ProdutoViewSetTestCase(TestCase):
 
         self.assertEqual(response.status_code, HTTP_201_CREATED)
         self.assertEqual(data, expected)
+
+    @parameterized.expand(DATA_IN_ERROR)
+    def test_criar_produto_sem_campos_obrigatorios(self, data, data_error):
+        data.update({'categoria': self.categoria.pk})
+
+        response = self.client.post(self.class_router, data)
+        msg = response.data.get(data_error.get('campo'))[0].capitalize()
+
+        self.assertEqual(response.status_code, HTTP_400_BAD_REQUEST)
+        self.assertEqual(data_error.get('msg'), msg)
