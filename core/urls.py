3fda17020d9e1f3cb457fbe486bb72296f3c9407
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-20 17:57:08 -0300

    importando rotas para CategoriasViewSet

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
-from apps.produtos.views import ProdutoViewSet
+from apps.produtos.views import ProdutoViewSet, CategoriaViewSet
 
 
 routers = SimpleRouter()
 
 routers.register(r'produtos', ProdutoViewSet, basename='produtos')
+routers.register(r'produtos/categorias', CategoriaViewSet, basename='categorias')
 
 urlpatterns = [
     path('api/', include(routers.urls), name='api'),
