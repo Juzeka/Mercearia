6fa6af324da36f2e434fea59a188fb04a0e14bc6
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-20 20:40:26 -0300

    add rota para views de caixa

--- core/urls.py
+++ core/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from apps.produtos.views import ProdutoViewSet, CategoriaViewSet
-
+from apps.financeiro.views import CaixaViewSet
 
 routers = SimpleRouter()
 
 routers.register(r'produtos', ProdutoViewSet, basename='produtos')
 routers.register(r'categorias', CategoriaViewSet, basename='categorias')
+routers.register(r'financeiro/caixas', CaixaViewSet, basename='caixas')
 
 urlpatterns = [
     path('api/', include(routers.urls), name='api'),
