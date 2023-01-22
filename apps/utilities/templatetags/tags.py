5a41af028841c426ed77c4ef0ee3338c0b0c576d
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-22 16:43:26 -0300

    refatorando tags

--- apps/utilities/templatetags/tags.py
+++ apps/utilities/templatetags/tags.py
@@ -9,12 +9,14 @@ def parse_for_reais(value:Decimal):
     if value is None:
         return '-'
 
-    return 'R${:,.2f}'.format(value).replace(',', 'X').replace(
+    return 'R$ {:,.2f}'.format(value).replace(',', 'X').replace(
         '.', ','
     ).replace('X', '.')
 
 @register.filter
 def zeros_esquerda(value):
+    value = str(value) if value != 0 else value
+
     if len(value) == 4:
         value = f'0{value}'
     elif len(value) == 3:
@@ -25,3 +27,5 @@ def zeros_esquerda(value):
         value = f'0000{value}'
     else:
         value = f'0000{value}'
+
+    return value
