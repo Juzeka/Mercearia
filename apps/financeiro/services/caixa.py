2653e29b551c75b4c4617df92ef2d4f2ef62a7f1
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-22 20:52:36 -0300

    finalizando testes de relatorios diarios de fechamento

--- apps/financeiro/services/caixa.py
+++ apps/financeiro/services/caixa.py
@@ -1,14 +1,47 @@
 from rest_framework.views import Response
 from rest_framework.status import HTTP_400_BAD_REQUEST
+from django.shortcuts import render
+from django.template.loader import get_template
+from django.shortcuts import get_object_or_404
+from weasyprint import HTML
+from apps.financeiro.models import Caixa
+from apps.produtos.models import ProdutoOrigem
 
 
 class CaixaServices:
+    class_model = Caixa
+    template_name = 'financeiro/relatorio.html'
+
     def __init__(self, *args, **kwargs):
         self.serializer = kwargs.get('serializer')
         self.msg = kwargs.get('msg')
+        self.request = kwargs.get('request')
+        self.data = kwargs.get('data')
+        self.pk = kwargs.get('pk')
 
     def response_bad_request(self, condicao):
         if condicao:
             return Response({'msg': self.msg}, status=HTTP_400_BAD_REQUEST)
 
         return Response(self.serializer.data)
+
+    def get_data_relatorio(self):
+        return {
+            'caixa': get_object_or_404(
+                self.class_model,
+                pk=self.pk,
+                aberto=False
+            ),
+            'produtos': ProdutoOrigem.objects.all()
+        }
+
+    def gerar_relatorio(self):
+        return render(self.request, self.template_name, self.data)
+
+    def relatorio_fechamento_pdf(self):
+        template = get_template(self.template_name).render(self.data).encode(
+            encoding='UTF-8'
+        )
+
+        return HTML(string=template).write_pdf()
+
--- tests/financeiro/services/caixa.py
+++ tests/financeiro/services/caixa.py
@@ -3,10 +3,13 @@ from rest_framework.status import (
     HTTP_200_OK,
     HTTP_400_BAD_REQUEST,
 )
+from django.http.response import HttpResponse
 from apps.financeiro.models import Caixa
 from apps.financeiro.factories import CaixaFactory
 from apps.financeiro.services import CaixaServices
 from apps.financeiro.serializers import CaixaSerializer
+from apps.produtos.factories import CategoriaFactory, ProdutoOrigemFactory
+from apps.produtos.models import ProdutoOrigem
 from parameterized import parameterized
 
 
@@ -20,6 +23,11 @@ ABERTO_MSG_STATUS = [
     (False, None, False, HTTP_200_OK,),
     (False, 'Não é possível ver caixa fechado.', True, HTTP_400_BAD_REQUEST,),
     (True, None, True, HTTP_200_OK,),
+]
+HAS_PRODUTO = [(False,), (True,), (None,),]
+CONTENT_TYPE_HTML = [
+    ('application/pdf', False),
+    ('text/html; charset=utf-8', True),
 ]
 
 
@@ -54,3 +62,52 @@ class CaixaServicesTestCase(TestCase):
             self.assertEqual(msg, result.data.get('msg'))
 
         self.assertEqual(result.status_code, status)
+
+    @parameterized.expand(HAS_PRODUTO)
+    def test_pegar_dados_do_relatorio(self, has_produto):
+        data = self.data.copy()
+        data.update({'aberto': False})
+
+        caixa = self.class_factory(**data)
+
+        if has_produto:
+            ProdutoOrigemFactory.create_batch(
+                size=2,
+                valor=14.0,
+                categoria=CategoriaFactory()
+            )
+
+        result = self.class_services(pk=caixa.pk).get_data_relatorio()
+
+        exists_produtos = self.assertTrue if has_produto else self.assertFalse
+
+        exists_produtos(result.get('produtos').exists())
+        self.assertIsInstance(result.get('caixa'), self.class_model)
+
+    @parameterized.expand(CONTENT_TYPE_HTML)
+    def test_gerar_relatorio(self, content_type, is_html):
+        data = self.data.copy()
+        data.update({'aberto': False})
+
+        ProdutoOrigemFactory.create_batch(
+            size=2,
+            valor=14.0,
+            categoria=CategoriaFactory()
+        )
+
+        caixa = CaixaFactory(**data)
+        dados = {'caixa': caixa, 'produtos': ProdutoOrigem.objects.all()}
+
+        services = self.class_services(data=dados)
+
+        if is_html:
+            result = services.gerar_relatorio()
+
+            self.assertEqual(result.status_code, HTTP_200_OK)
+            self.assertEqual(result.headers.get('Content-type'), content_type)
+        else:
+            result = services.relatorio_fechamento_pdf()
+            response = HttpResponse(result, content_type)
+
+            self.assertIsInstance(response, HttpResponse)
+            self.assertEqual(response.status_code, HTTP_200_OK)
