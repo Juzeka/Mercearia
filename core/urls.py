96fdd4138092740fc83428fd47ff3cbc13e0cbc1
Juzeka <rafaelgomesalmeida@hotmail.com>
2023-01-22 23:14:02 -0300

    add viewser de sangria

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from apps.produtos.views import ProdutoViewSet, CategoriaViewSet
-from apps.financeiro.views import CaixaViewSet
+from apps.financeiro.views import CaixaViewSet, SangriaViewSet
 
 
 routers = DefaultRouter()
@@ -9,6 +9,7 @@ routers = DefaultRouter()
 routers.register(r'produtos', ProdutoViewSet, basename='produtos')
 routers.register(r'categorias', CategoriaViewSet, basename='categorias')
 routers.register(r'financeiro/caixas', CaixaViewSet, basename='caixas')
+routers.register(r'financeiro/sangrias', SangriaViewSet, basename='sangrias')
 
 urlpatterns = [
     path('api/', include(routers.urls), name='api'),
